8123dfca3d4b4ec5bdf7ebd4447072ec13c90b27
Vladislav-korobeynikov <Vladislav.korobeynikov@yandex.ru>
2021-05-15 17:11:47 +0400

    Первое задание
    Готовая программа

---
+++ UnityTest/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string flag = "+";
+            int crutch = 0;
+
+
+            // инвентарь и кошелек игрока
+            int wallet = 0;
+            List<string> inventory = new List<string>();
+            int choic;
+
+            Road road = new Road();
+            do
+            {
+                do
+                {
+                    choic = Convert.ToInt32(Console.ReadLine());
+                    if (choic == 1 || choic == 2)
+                    {
+                        flag = "-";
+                        switch (choic)
+                        {
+                            case 1:
+                                if (crutch == 0)
+                                {
+                                    road.GetActionOneInfo();
+                                    wallet += road.GetActionMoney();
+                                    inventory.Add(road.GetActionLocket());
+                                    crutch++;
+                                }
+                                else
+                                {
+                                    road.GetActionOneTwoInfo();
+                                }
+
+                                break;
+
+                            case 2:
+                                road.GetActionTwoInfo();
+
+                                break;
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный выбор! Попробуйте ещё!");
+                    }
+                }
+                while (flag == "+");
+                flag = "+";
+            }
+            while (flag == "+");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UnityTest/Room/Road.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityTest
+{
+    class Road
+    {
+
+        public Road()
+        {
+            Console.WriteLine("Вы находитесь на локации Дорога\nВаши действия");
+            Console.WriteLine("1. Поговорить с бродягой");
+            Console.WriteLine("2. Осмотреть окрестности");
+        }
+
+        public void GetActionOneInfo()
+        {
+            Console.WriteLine("Получаете от бродяги 50 монет и медальон");
+        }
+
+        public int GetActionMoney() //метод для выдачи 50 монет
+        {
+            return 50;
+        }
+
+        public string GetActionLocket() // метод для выдачи медальона
+        {
+            return "медальон";
+        }
+
+        public void GetActionOneTwoInfo()
+        {
+            Console.WriteLine("Бродяга молчит");
+        }
+
+        public void GetActionTwoInfo()
+        {
+            Console.WriteLine("Вы осматриваете местность");
+        }
+
+    }
+}
